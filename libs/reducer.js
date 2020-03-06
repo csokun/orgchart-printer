a18b2ed8d61c3053dfedcda4bf2b1662428b2c07
Sokun Chorn <chornsokun@gmail.com>
2020-03-07 01:02:53 +1100

    self-manage

--- libs/reducer.js
+++ libs/reducer.js
@@ -1,26 +1,37 @@
+const selfManage = 99999;
+
+function createDefaultManager({ managerId }) {
+  const name = (managerId === selfManage ? '** self-manage **' : '** manager unknown **');
+  return { id: managerId, name , managerId: null, subordinates: [] };
+}
+
 const reducer = ({ employees }) => {
   const classifiedEmployees = [];
 
   employees.reduce((accumulated, employee) => {
-    const { id, name, managerId } = employee;
+    if (employee.id === employee.managerId)
+      employee.managerId = selfManage;
+
     if (!employee.subordinates) employee.subordinates = [];
+
+    const { id, name, managerId } = employee;
 
     if (!accumulated[id]) {
       accumulated[id] = employee;
       classifiedEmployees.push(accumulated[id]);
     } else
       Object.assign(accumulated[id], { name, managerId });
-  
+
     if (managerId) {
       let manager = accumulated[managerId];
       if (!manager) {
-        manager = { id: managerId, name: '** self-manager **', managerId: null, subordinates: [] };
+        manager = createDefaultManager({ managerId });
         accumulated[managerId] = manager;
         classifiedEmployees.push(manager);
       }
       manager.subordinates.push(employee);
     }
-  
+
     return accumulated;
   }, { });
 
